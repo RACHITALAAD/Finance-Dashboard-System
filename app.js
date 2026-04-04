e46d1d10da473788dbfe151e687b63442dec9d74
Rachita Laad <rachitalaad505@gmail.com>
2026-04-04 10:45:00 +0530

    add route

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Finance Backend API is Live');
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/records', recordsRoutes);
